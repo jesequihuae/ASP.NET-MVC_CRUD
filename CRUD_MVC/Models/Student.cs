a8d2cef9180db12ed7c118ca90d8035cd809d9e2
Your Name <jesequihuae@gmail.com>
2019-12-22 01:08:25 -0500

    complete

--- CRUD_MVC/Models/Student.cs
+++ CRUD_MVC/Models/Student.cs
@@ -6,11 +6,20 @@ namespace CRUD_MVC.Models
     public class Student
     {
         public int StudentID { get; set; }
+
         [Required(ErrorMessage = "Name is required*")]
+        [DataType(DataType.Text)]
+        [MaxLength(150)]
         public String Name { get; set; }
+
         [Required(ErrorMessage = "Last Name is required*")]
+        [DataType(DataType.Text)]
+        [MaxLength(150)]
         public String LastName { get; set; }
+
         [Required(ErrorMessage = "Date of Birth is required*")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd MMM yyyy}")]
         public DateTime DOB { get; set; }
 
         public Student() { }
